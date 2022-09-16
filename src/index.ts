3907319ca67f7b14611e91d0fd3d9632ac082657
Kaycodes11 <kayydraws@gmail.com>
2022-09-16 22:08:47 +0530

    node typescipt basic setup done: use/make branch from here

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,41 @@
-import express, { Application, Request, Response } from "express";
+import express, {
+  Application,
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
+import { Server } from "http";
+import { config } from "dotenv";
+import createHttpError from "http-errors";
 
 const app: Application = express();
-const PORT = 5000;
+
+config();
+
+const PORT = Number(process.env.PORT);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async (req: Request, res: Response): Promise<Response> => {
+app.get("/", (req: Request, res: Response, next: NextFunction): Response => {
   return res.status(200).json({ message: "Homepage" });
 });
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new createHttpError.NotFound());
+});
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  res.send({ status: err.status || 500, message: err.message });
+};
+
+app.use(errorHandler);
+
 try {
-  app.listen(PORT, (): void => {
+  const server: Server = app.listen(PORT, (): void => {
     console.log(`server started on the ${PORT}`);
   });
-} catch (e) {}
+} catch (e: any) {
+  console.log(e.message);
+}
